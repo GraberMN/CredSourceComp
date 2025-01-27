4f75ec5844153378fc0d9fc4f04d9c9366ee0b15
Mateo Graber <grabermn3@gmail.com>
2025-01-27 13:15:40 -0500

    commit on "main"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import Embed from './components/Embed.js';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 import SourceSelect from './components/SourceSelect.js';
+
+const initialState = { embed3Display: 'none', embed4Display: 'none' }
+const reducer = (state, action) => {
+  switch (action) {
+    case '2':
+      return initialState
+    case '3':
+      return { embed3Display: 'flex', embed4Display: 'none' }
+    case '4':
+      return { embed3Display: 'flex', embed4Display: 'flex' }
+    default:
+      return state
+  }
+}
 
 function App() {
   const [sourceNum, setSourceNum] = useState(2)
-  const [newSourceNum, setNewSourceNum] = useState(false)
+  const [newSourceNum, setNewSourceNum] = useState(2)
+  const [displayEmbed, dispatch] = useReducer(reducer, initialState)
+  const [embed3Style, setEmbed3Style] = useState({ display: 'none' })
+  const [embed4Style, setEmbed4Style] = useState({ display: 'none' })
   useEffect(() => {
-    console.log(sourceNum)
+    let newEmbed3Style
+    let newEmbed4Style
+    if (newSourceNum === 2) {
+      newEmbed3Style = { display: 'none' }
+      newEmbed4Style = { display: 'none' }
+    } else if (newSourceNum === 3) {
+      newEmbed3Style = { display: 'flex' }
+      newEmbed4Style = { display: 'none' }
+    } else {
+      newEmbed3Style = { display: 'flex' }
+      newEmbed4Style = { display: 'flex' }
+    }
+    setEmbed3Style(newEmbed3Style)
+    setEmbed4Style(newEmbed4Style)
+    console.log(embed3Style)
+    console.log(embed4Style)
   }, [newSourceNum])
   return (
     <div className="App">
@@ -20,8 +52,8 @@ function App() {
         <Embed className="embed" title="embed2" />
       </span>
       <span id="embedWrapperRow2">
-        <Embed className="embed" title="embed3" />
-        <Embed className="embed" title="embed4" />
+        <Embed className="embed" title="embed3" style={embed3Style} />
+        <Embed className="embed" title="embed4" style={embed4Style} />
       </span>
     </div>
   );
--- src/components/SourceSelect.js
+++ src/components/SourceSelect.js
@@ -6,11 +6,23 @@ function SourceSelect({ sourceNum, setSourceNum, newSourceNum, setNewSourceNum }
         <div>
             <span>How many sources? (2-4) </span>
             <span id='sourceNumSelect'>
-                <span className='option' id='option1' onClick={() => setSourceNum(2)}>2</span>
-                <span className='option' id='option2' onClick={() => setSourceNum(3)}>3</span>
-                <span className='option' id='option3' onClick={() => setSourceNum(4)}>4</span>
+                <span className='option' id='option1' onClick={() => {
+                    setSourceNum(2) 
+                    console.log(sourceNum)
+                }}>2</span>
+                <span className='option' id='option2' onClick={() => {
+                    setSourceNum(3)
+                    console.log(sourceNum)
+                }}>3</span>
+                <span className='option' id='option3' onClick={() => {
+                    setSourceNum(4)
+                    console.log(sourceNum)
+                }}>4</span>
             </span>
-            <button type='button' onClick={() => setNewSourceNum(!newSourceNum)}>Confirm</button>
+            <button type='button' onClick={() => {
+                setNewSourceNum(sourceNum)
+                console.log(newSourceNum)
+            }}>Confirm</button>
         </div>
     )
 }
